08e20d45515b2fc9da71ac494b6e4bb855f0d01d
Colin Miller <colin.miller@mrjaffesclass.com>
2023-01-06 11:01:27 -0800

    l

--- ConnectFour/ConnectFour.java
+++ ConnectFour/ConnectFour.java
@@ -11,7 +11,7 @@ public class ConnectFour {
 
     public ConnectFour() {
         System.out.println("--Use the playConnectFour method to begin game--");
-        System.out.println("-- 0represents an empty spot -- 1 represents player1's tiles -- 2 represents player2's tiles --");
+        System.out.println("-- 0 represents an empty spot -- 1 represents player1's tiles -- 2 represents player2's tiles --");
         System.out.println("the game shall begin with player1's move --- type the number for the column you wish to place your tile");
 
         this.LOWER_BOUND = 1;
@@ -33,12 +33,14 @@ public class ConnectFour {
         }
 
         this.gameIsRunning = true;
-        System.out.println("|" + gameboard[0][0] + "|" + gameboard[0][1] + "|" + gameboard[0][2] + "|" + gameboard[0][3] + "|" + gameboard[0][4] + "|" + gameboard[0][5] + "|" + gameboard[0][6] + "|");
-        System.out.println("|" + gameboard[1][0] + "|" + gameboard[1][1] + "|" + gameboard[1][2] + "|" + gameboard[1][3] + "|" + gameboard[1][4] + "|" + gameboard[1][5] + "|" + gameboard[1][6] + "|");
-        System.out.println("|" + gameboard[2][0] + "|" + gameboard[2][1] + "|" + gameboard[2][2] + "|" + gameboard[2][3] + "|" + gameboard[2][4] + "|" + gameboard[2][5] + "|" + gameboard[2][6] + "|");
-        System.out.println("|" + gameboard[3][0] + "|" + gameboard[3][1] + "|" + gameboard[3][2] + "|" + gameboard[3][3] + "|" + gameboard[3][4] + "|" + gameboard[3][5] + "|" + gameboard[3][6] + "|");
-        System.out.println("|" + gameboard[4][0] + "|" + gameboard[4][1] + "|" + gameboard[4][2] + "|" + gameboard[4][3] + "|" + gameboard[4][4] + "|" + gameboard[4][5] + "|" + gameboard[4][6] + "|");
+        
         System.out.println("|" + gameboard[5][0] + "|" + gameboard[5][1] + "|" + gameboard[5][2] + "|" + gameboard[5][3] + "|" + gameboard[5][4] + "|" + gameboard[5][5] + "|" + gameboard[5][6] + "|");
+        System.out.println("|" + gameboard[4][0] + "|" + gameboard[4][1] + "|" + gameboard[4][2] + "|" + gameboard[4][3] + "|" + gameboard[4][4] + "|" + gameboard[4][5] + "|" + gameboard[4][6] + "|");
+        System.out.println("|" + gameboard[3][0] + "|" + gameboard[3][1] + "|" + gameboard[3][2] + "|" + gameboard[3][3] + "|" + gameboard[3][4] + "|" + gameboard[3][5] + "|" + gameboard[3][6] + "|");
+        System.out.println("|" + gameboard[2][0] + "|" + gameboard[2][1] + "|" + gameboard[2][2] + "|" + gameboard[2][3] + "|" + gameboard[2][4] + "|" + gameboard[2][5] + "|" + gameboard[2][6] + "|"); 
+        System.out.println("|" + gameboard[1][0] + "|" + gameboard[1][1] + "|" + gameboard[1][2] + "|" + gameboard[1][3] + "|" + gameboard[1][4] + "|" + gameboard[1][5] + "|" + gameboard[1][6] + "|");
+        System.out.println("|" + gameboard[0][0] + "|" + gameboard[0][1] + "|" + gameboard[0][2] + "|" + gameboard[0][3] + "|" + gameboard[0][4] + "|" + gameboard[0][5] + "|" + gameboard[0][6] + "|");
+        
         System.out.println(" ");
         System.out.println("-" + "1" + "-" + "2" + "-" + "3" + "-" + "4" + "-" + "5" + "-" + "6" + "-" + "7" + "-");
 
@@ -56,15 +58,17 @@ public class ConnectFour {
                         
                     }
                     
-                    System.out.println("|" + gameboard[0][0] + "|" + gameboard[0][1] + "|" + gameboard[0][2] + "|" + gameboard[0][3] + "|" + gameboard[0][4] + "|" + gameboard[0][5] + "|" + gameboard[0][6] + "|");
-                    System.out.println("|" + gameboard[1][0] + "|" + gameboard[1][1] + "|" + gameboard[1][2] + "|" + gameboard[1][3] + "|" + gameboard[1][4] + "|" + gameboard[1][5] + "|" + gameboard[1][6] + "|");
-                    System.out.println("|" + gameboard[2][0] + "|" + gameboard[2][1] + "|" + gameboard[2][2] + "|" + gameboard[2][3] + "|" + gameboard[2][4] + "|" + gameboard[2][5] + "|" + gameboard[2][6] + "|");
-                    System.out.println("|" + gameboard[3][0] + "|" + gameboard[3][1] + "|" + gameboard[3][2] + "|" + gameboard[3][3] + "|" + gameboard[3][4] + "|" + gameboard[3][5] + "|" + gameboard[3][6] + "|");
-                    System.out.println("|" + gameboard[4][0] + "|" + gameboard[4][1] + "|" + gameboard[4][2] + "|" + gameboard[4][3] + "|" + gameboard[4][4] + "|" + gameboard[4][5] + "|" + gameboard[4][6] + "|");
                     System.out.println("|" + gameboard[5][0] + "|" + gameboard[5][1] + "|" + gameboard[5][2] + "|" + gameboard[5][3] + "|" + gameboard[5][4] + "|" + gameboard[5][5] + "|" + gameboard[5][6] + "|");
+                    System.out.println("|" + gameboard[4][0] + "|" + gameboard[4][1] + "|" + gameboard[4][2] + "|" + gameboard[4][3] + "|" + gameboard[4][4] + "|" + gameboard[4][5] + "|" + gameboard[4][6] + "|");
+                    System.out.println("|" + gameboard[3][0] + "|" + gameboard[3][1] + "|" + gameboard[3][2] + "|" + gameboard[3][3] + "|" + gameboard[3][4] + "|" + gameboard[3][5] + "|" + gameboard[3][6] + "|");
+                    System.out.println("|" + gameboard[2][0] + "|" + gameboard[2][1] + "|" + gameboard[2][2] + "|" + gameboard[2][3] + "|" + gameboard[2][4] + "|" + gameboard[2][5] + "|" + gameboard[2][6] + "|"); 
+                    System.out.println("|" + gameboard[1][0] + "|" + gameboard[1][1] + "|" + gameboard[1][2] + "|" + gameboard[1][3] + "|" + gameboard[1][4] + "|" + gameboard[1][5] + "|" + gameboard[1][6] + "|");
+                    System.out.println("|" + gameboard[0][0] + "|" + gameboard[0][1] + "|" + gameboard[0][2] + "|" + gameboard[0][3] + "|" + gameboard[0][4] + "|" + gameboard[0][5] + "|" + gameboard[0][6] + "|");
+                    
                     System.out.println(" ");
                     System.out.println("-" + "1" + "-" + "2" + "-" + "3" + "-" + "4" + "-" + "5" + "-" + "6" + "-" + "7" + "-");
                 }
+                
             }
             catch (InputMismatchException error) {
                 System.out.println("please input a number between 1 - 7");
